894ab224bd3d9c1d00c7ac26a7945b8c66b58615
Gilli-Cheng <gillibeanbean00@gmail.com>
2021-07-15 23:21:33 +0800

    assignment 2 completed!

--- cachematrix.R
+++ cachematrix.R
@@ -6,20 +6,24 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+makeCacheMatrix <- function(x = matrix()) { ## default matrix data type
+        m <- NULL                           ## m as the empty basket for the later inv calculation
         set <- function(y) {
-                x <<- y
+                x <<- y                     ## assign imput matrix in parent level; reset m
                 m <<- NULL
-        }
-        get <- function() x
-        setsol <- function(solve) m <<- solve
-        getsol <- function() m
-        list(set = set, get = get,
-             setsol = setsol,
-             getsol = getsol)
+        }                                   
+        get <- function() x                 ## returns input matrix
+        
+        
+        setsol <- function(solve) m <<- solve   ##  assign m in parent level
+        getsol <- function() m                  ##  the value of func cacheSolve will save back here
+        
+        
+        list(set = set, get = get,setsol = setsol,getsol = getsol)
+        ## assign var name for using $ operator later on
 }
 
+## cacheSolve will do the inverse calculation
 
 cacheSolve <- function(x, ...) {
         m <- x$getsol()
@@ -29,6 +33,6 @@ cacheSolve <- function(x, ...) {
         }
         data <- x$get()
         m <- solve(data, ...)
-        x$setsol(m)
-        m## Return a matrix that is the inverse of 'x'
+        x$setsol(m)                          ## put value in cache; look up there !!
+        m                                    ## Return a matrix that is the inverse of 'x'
 }
