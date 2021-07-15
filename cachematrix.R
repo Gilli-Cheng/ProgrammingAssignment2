7e7167262a545f249b767f520a86b50ac60c4996
Gilli-Cheng <gillibeanbean00@gmail.com>
2021-07-15 22:39:09 +0800

    week3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programming week3 assignment
 
-## Write a short comment describing this function
+## the makeCacheMatrix func is a parent function
+## the <- works on the current level
+## <<- can modify var in the parent level
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsol <- function(solve) m <<- solve
+        getsol <- function() m
+        list(set = set, get = get,
+             setsol = setsol,
+             getsol = getsol)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsol()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsol(m)
+        m## Return a matrix that is the inverse of 'x'
 }
